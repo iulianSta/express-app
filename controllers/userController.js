0dd80d4f9e73eec9d2c47a2ac5ee6b879b9883f6
iulianstan <iulian1226@gmail.com>
2021-08-26 15:03:34 +0200

    Update userController.js - creating getUser middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,3 +2,22 @@ const userData = require("../model/userModel");
 const express = require("express");
 
 // Middleware
+// Get one user by username
+const getUser = async (req, res, next) => {
+  let user;
+  try {
+    // user = await EmployeesData.findById(req.params.id);
+    // user = await EmployeesData.find({ name: req.params.name });
+    user = await UserData.findOne({ userName: req.params.userName });
+    console.log(user);
+    if (user == null) {
+      // NOt found
+      return res.status(404).json({ message: "Username not found" });
+    }
+  } catch (err) {
+    // 500 Internal server error
+    res.status(500).json({ message: err.message });
+  }
+  res.user = user;
+  next();
+};
