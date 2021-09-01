b18690816bdcc9119fa911f97bb1433488491a88
iulianstan <iulian1226@gmail.com>
2021-09-01 15:14:27 +0200

    update router/user.js and display.js

--- router/user.js
+++ router/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controller/userController");
+const userController = require("../controllers/userController");
 const userMiddleware = require("../middleware/");
 
 const { checkUserData, checkAge, checkFbw, getUser } = userMiddleware;
