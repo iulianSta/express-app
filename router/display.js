6cc2e0eba43219970f2f902df5486a322a872db4
iulianstan <iulian1226@gmail.com>
2021-09-01 14:58:48 +0200

    create display.js

---
+++ router/display.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controller/userController");
+const userMdd = require("../middleware/");
+
+const {
+  getUser,
+  userNameCapitalized,
+  toolStackArrSort,
+  strToNum,
+  displayUser,
+} = userMiddleware;
+
+// GET one user
+// URL  http://localhost:5000/display/:name
+
+router
+  .route("/:name")
+  .get(
+    getUser,
+    userNameCapitalized,
+    toolStackArrSort,
+    strToNum,
+    userController.displayUser
+  );
+
+module.exports = router;
